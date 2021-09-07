5e2904785d846909900b8df6d9eede8ec51f119e
piyush <piyushgk1@gmail.com>
2021-09-07 15:27:24 -0400

    update manifest to v3, and reduce time between changing favicon

--- replace.js
+++ replace.js
@@ -3,9 +3,6 @@ function changeFaviconHref(){
     $('link[rel="shortcut icon"]').attr('href','/images/favicon.ico');
 }
 
-// set favicon after notion has finished dynmaicaaly assigning favicon
-setTimeout(changeFaviconHref, 2000);
-
 //continue to change href in case notion tries to change favicon again
-setInterval(changeFaviconHref, 10000);
+setInterval(changeFaviconHref, 2000);
 
